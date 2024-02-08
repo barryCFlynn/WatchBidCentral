8793529729b1a50ba7e89b7318219b79ce712611
barry flynn <barry.c.flynn@gmail.com>
2024-02-08 21:43:40 +0000

    readies code for heroku

--- watchbidcentral/settings.py
+++ watchbidcentral/settings.py
@@ -23,9 +23,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-)-8)se1#a8zb8k+4m-1(kqdb@w995qrt(+mhupoc0)ety&bvxk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ["8000-barrycflynn-watchbidcen-wl2gcpiqcxa.ws-eu108.gitpod.io"]
+ALLOWED_HOSTS = [
+    "8000-barrycflynn-watchbidcen-wl2gcpiqcxa.ws-eu108.gitpod.io",
+    ".herokuapp.com",
+]
 
 
 # Application definition
