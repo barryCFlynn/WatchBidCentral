23bdae9e4e39fa224261929d9da89b21436ad603
barry flynn <barry.c.flynn@gmail.com>
2024-02-12 12:21:43 +0000

    bug rename profile details for heroku deploy

--- watchbidcentral/settings.py
+++ watchbidcentral/settings.py
@@ -27,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-barrycflynn-watchbidcen-wl2gcpiqcxa.ws-eu108.gitpod.io",
