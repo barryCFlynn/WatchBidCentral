82450998326c66e6bf0f3f05ec14ec50d9f70a4f
barry flynn <barry.c.flynn@gmail.com>
2024-02-17 17:39:20 +0000

    Add boostrap template and css to base and index html

--- buy/urls.py
+++ buy/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.Listings.as_view(), name='home'),
+    path('', views.ListingsList.as_view(), name='home'),
 ]
--- buy/views.py
+++ buy/views.py
@@ -4,7 +4,7 @@ from .models import Listing
 
 
 # Create your views here.
-class Listings(generic.ListView):
+class ListingsList(generic.ListView):
     queryset = Listing.objects.filter(status=1)
     template_name = "buy/index.html"
     paginate_by = 6
