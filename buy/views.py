5c04cb23ff6bfb0f61602829901f747c5101c86a
barry flynn <barry.c.flynn@gmail.com>
2024-02-19 16:03:17 +0000

    Fix bug of template not pulling database info

--- buy/views.py
+++ buy/views.py
@@ -7,4 +7,4 @@ from .models import Listing
 class ListingsList(generic.ListView):
     queryset = Listing.objects.filter(status=1)
     template_name = "buy/index.html"
-    paginate_by = 6
+    paginate_by = 8
